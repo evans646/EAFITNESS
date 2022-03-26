c1fdb0674da5f6b1def584d7c101c55b0d72ff47
Evans Ansong <65941289+evans646@users.noreply.github.com>
2022-03-26 17:59:38 +0000

    local file server commit message

--- plans/static/js/index.js
+++ plans/static/js/index.js
@@ -1,10 +1,10 @@
-$(document).on("scroll", function(){
-    if
-  ($(document).scrollTop() > 100){
-      $("header").addClass("shrink");
-    }
-    else
-    {
-        $("header").removeClass("shrink");
-    }
-});
+// $(document).on("scroll", function(){
+//     if
+//   ($(document).scrollTop() > 100){
+//       $("header").addClass("shrink");
+//     }
+//     else
+//     {
+//         $("header").removeClass("shrink");
+//     }
+// });
