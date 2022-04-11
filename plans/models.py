b7303e19d0b140320d7042358bddcf6a341f1be5
Evans Ansong <65941289+evans646@users.noreply.github.com>
2022-04-11 08:41:54 +0000

    initial commit

--- plans/models.py
+++ plans/models.py
@@ -15,7 +15,6 @@ class FitnessPlan(models.Model):
     def __str__(self):
            return self.title
 
-
 class Fitness(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
--- plans/static/js/index.js
+++ plans/static/js/index.js
@@ -8,23 +8,6 @@ $(function(){
       }
     });
     
-    $(function(){
-      //the shrinkHeader variable is where you tell the scroll effect to start.
-      var shrinkHeader = 70;
-       $(window).scroll(function() {
-         var scroll = getCurrentScroll();
-           if ( scroll >= shrinkHeader ) {
-                $('.header-box').addClass('smaller');
-             }
-             else {
-                 $('.header-box').removeClass('smaller');
-             }
-       });
-     function getCurrentScroll() {
-         return window.pageYOffset || document.documentElement.scrollTop;
-         }
-     });
-
 
   $(window).scroll(function() {
     if ($(this).scrollTop() > 50 ) {
